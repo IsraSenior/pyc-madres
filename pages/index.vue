7227a00bfd67e7fd5a8e2b4825cfbf60bb2f04fb
Israel Senior <israsenior.dev@gmail.com>
2025-07-29 23:21:21 -0400

    _push

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="bg-[#fef9f3]">
+    <div class="bg-[#fef9f3] relative isolate">
+        <img src="/background.webp" class="bg-cover bg-center absolute inset-0 h-full w-full" alt="">
+
         <section class="relative lg:py-8 h-screen flex items-center justify-center overflow-hidden">
             <div
-                class="container isolate h-full flex items-center justify-center transform overflow-hidden shadow bg-[#eee5d4] relative">
-                <div class="border border-primary absolute inset-5"></div>
+                class="container isolate h-full flex items-center justify-center transform overflow-hidden shadow glass relative lg:rounded-xl">
+                <div class="border border-primary absolute inset-5 rounded-lg"></div>
 
                 <div class="inset-0 absolute bg-gradient-to-r from-0% from-secondary-900 to-70% to-transparent">
                 </div>
 
-                <div class="max-w-2xl text-primary p-12 relative text-center">
+                <div class="max-w-4xl text-primary p-12 relative text-center">
                     <div class="flex space-x-6 items-center justify-center mb-12">
-                        <Logo class="h-24 lg:h-28" />
+                        <Logo class="h-20 md:h-24 lg:h-28" />
 
                     </div>
                     <div class="space-y-6 mt-6 font-houschka text-2xl lg:text-3xl font-normal">
@@ -18,35 +20,59 @@
                             Te esperamos este próximo <br>
                             <b>Viernes 9 de Mayo</b> <br> en nuestro salón
                         </p> -->
-                        <h1 class="text-5xl lg:text-7xl font-normal font-agilera uppercase">
+                        <!-- <h1 class="text-5xl lg:text-7xl font-normal font-agilera uppercase">
                             Mes de los <br>
                             <span class="text-6xl lg:text-8xl">Padres</span>
                         </h1>
                         <p>
                             ¡Disfrutemos juntos de una copa!
-                        </p>
+                        </p> -->
                     </div>
                     <div
                         class="space-x-0 md:space-x-6 flex flex-col md:flex-row space-y-3 md:space-y-0 items-center justify-center w-full mt-12">
                         <button type="button" @click.prevent="openModal('salones')"
-                            class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
-                            Nuestros salones</button>
+                            class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base cursor-pointer font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Eventos privados</button>
 
                         <!-- <button type="button" @click.prevent="openModal('seleccion')"
                             class="bg-[#d77935] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
                             Selección Especial
                         </button> -->
 
-                        <a href="https://wa.me/18294232486" target="_blank" type="button"
+                        <a href="/carta-vinos-pyc-.pdf" download="Carta de vinos y destilados del Winebar"
+                            target="_blank"
+                            class="bg-[#302f2f] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Carta del Winebar
+                        </a>
+
+                        <!-- <a href="https://wa.me/18294232486" target="_blank"
                             class="bg-[#302f2f] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
                             Reserva ahora
+                        </a> -->
+
+                    </div>
+
+                    <div
+                        class="space-x-0 md:space-x-6 flex flex-col md:flex-row space-y-3 md:space-y-0 items-center justify-center w-full mt-12 border-t border-primary pt-12">
+
+                        <a href="https://wa.me/18294232486" target="_blank"
+                            class="bg-white w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-[#302f2f] shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Reserva tu evento
+                        </a>
+
+                        <a href="https://wa.me/18294232486" target="_blank"
+                            class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base cursor-pointer font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Siguenos en Instagram
+                        </a>
+
+                        <a href="https://wa.me/18294232486" target="_blank"
+                            class="bg-white w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-[#302f2f] shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Comunicarse con tienda
                         </a>
 
                     </div>
                 </div>
             </div>
-            <Flower1 class="absolute h-96 w-auto lg:h-full -top-20 -right-20 lg:-right-46" />
-            <Flower2 class="absolute hidden lg:block lg:h-full -bottom-32 -left-20" />
         </section>
 
         <div v-if="!!modalType" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -73,7 +99,8 @@
                                     @click.prevent="modalType = null">X</button>
                                 <Gallery v-if="modalType === 'salones'" />
                                 <div v-if="modalType === 'seleccion'" class="flex items-center justify-center">
-                                    <img class="h-[80vh] object-contain object-center" src="/canva-especial.webp" alt="">
+                                    <img class="h-[80vh] object-contain object-center" src="/canva-especial.webp"
+                                        alt="">
                                 </div>
                             </div>
                         </div>
