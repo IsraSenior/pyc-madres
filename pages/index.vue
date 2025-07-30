4b071aca4669b395a8e0681719357f3affd4deb6
Israel Senior <israsenior.dev@gmail.com>
2025-07-30 00:00:32 -0400

    _

--- pages/index.vue
+++ pages/index.vue
@@ -30,8 +30,14 @@
                     </div>
                     <div
                         class="space-x-0 md:space-x-6 flex flex-col md:flex-row space-y-3 md:space-y-0 items-center justify-center w-full mt-12">
-                        <button type="button" @click.prevent="openModal('salones')"
+
+                        <a href="https://www.instagram.com/puntoycorchord" target="_blank"
                             class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base cursor-pointer font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            Siguenos en Instagram
+                        </a>
+
+                        <button type="button" @click.prevent="openModal('salones')"
+                            class="bg-[#302f2f] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
                             Eventos privados</button>
 
                         <!-- <button type="button" @click.prevent="openModal('seleccion')"
@@ -41,7 +47,7 @@
 
                         <a href="/carta-vinos-pyc-.pdf" download="Carta de vinos y destilados del Winebar"
                             target="_blank"
-                            class="bg-[#302f2f] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            class="bg-white w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-[#302f2f] shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
                             Carta del Winebar
                         </a>
 
@@ -55,10 +61,7 @@
                     <div
                         class="space-x-0 md:space-x-6 flex flex-col md:flex-row space-y-3 md:space-y-0 items-center justify-center w-full mt-12 border-t border-primary pt-12">
 
-                        <a href="https://www.instagram.com/puntoycorchord" target="_blank"
-                            class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base cursor-pointer font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
-                            Siguenos en Instagram
-                        </a>
+                        
 
                         <a href="https://wa.me/18294232486" target="_blank"
                             class="bg-[#302f2f] w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
@@ -66,7 +69,7 @@
                         </a>
 
                         <a href="https://wa.me/18294232486" target="_blank"
-                            class="bg-white w-full lg:w-auto px-4 py-2.5 text-base font-semibold text-[#302f2f] shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
+                            class="bg-primary w-full lg:w-auto px-4 py-2.5 text-base cursor-pointer font-semibold text-white shadow-xs hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">
                             Comunicarse con tienda
                         </a>
 
